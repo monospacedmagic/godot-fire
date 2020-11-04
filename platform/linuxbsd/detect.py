fed7ede573750519241cd68b2530e28006e6a96f
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-11-04 12:21:14 -0800

    Elixir
    `scons-3 p=linuxbsd -j16 ta1rget=release_debug use_llvm=yes module_webm_enabled=no`

--- platform/linuxbsd/detect.py
+++ platform/linuxbsd/detect.py
@@ -87,6 +87,9 @@ def get_flags():
 
 def configure(env):
 
+    env.Append(CCFLAGS=["-fPIC"])
+    env.Append(LINKFLAGS=["-fPIC"])
+
     ## Build type
 
     if env["target"] == "release":
@@ -192,7 +195,6 @@ def configure(env):
     # -fpie and -no-pie is supported on GCC 6+ and Clang 4+, both below our
     # minimal requirements.
     env.Append(CCFLAGS=["-fpie"])
-    env.Append(LINKFLAGS=["-no-pie"])
 
     ## Dependencies
 
